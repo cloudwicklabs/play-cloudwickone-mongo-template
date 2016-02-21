b78e3e9b45eae30a8730e33540ef2086acf82b59
Ashrith Mekala <ashrith@me.com>
2016-02-20 19:15:39 -0800

    FIX SSO to allow email addresses from Cloudwick domain
    This patch will fix the SSO module to only users with Cloudwick email
    address to login while restricting other Google based email addresses.
    
    DMR-126 #resolve

--- app/controllers/SocialAuthController.scala
+++ app/controllers/SocialAuthController.scala
@@ -32,20 +32,27 @@ class SocialAuthController @Inject() (
   def authenticate(provider: String) = Action.async { implicit request =>
     (socialProviderRegistry.get[SocialProvider](provider) match {
       case Some(p: SocialProvider with CommonSocialProfileBuilder) =>
+
         p.authenticate().flatMap {
           case Left(result) => Future.successful(result)
-          case Right(authInfo) => for {
-            profile <- p.retrieveProfile(authInfo)
-            user <- userService.insert(profile)
-            authInfo <- authInfoRepository.save(profile.loginInfo, authInfo)
-            authenticator <- env.authenticatorService.create(profile.loginInfo)
-            value <- env.authenticatorService.init(authenticator)
-            result <- env.authenticatorService.embed(value, Redirect(routes.Application.index()))
-            // if profile.email.get.endsWith("cloudwick.com")
-          } yield {
-            env.eventBus.publish(LoginEvent(user, request, request2Messages))
-            result
-          }
+          case Right(authInfo) =>
+            p.retrieveProfile(authInfo).flatMap { profile =>
+              val email = profile.email.get
+              if (email.endsWith("cloudwick.com")) {
+                for {
+                  user <- userService.insert(profile)
+                  authInfo <- authInfoRepository.save(profile.loginInfo, authInfo)
+                  authenticator <- env.authenticatorService.create(profile.loginInfo)
+                  value <- env.authenticatorService.init(authenticator)
+                  result <- env.authenticatorService.embed(value, Redirect(routes.Application.index()))
+                } yield {
+                  env.eventBus.publish(LoginEvent(user, request, request2Messages))
+                  result
+                }
+              } else {
+                Future(Redirect(routes.Application.signIn()).flashing("error" -> Messages("Use Cloudwick Email Address.")))
+              }
+            }
         }
       case _ => Future.failed(new ProviderException(s"Cannot authenticate with unexpected social provider $provider"))
     }).recover {
